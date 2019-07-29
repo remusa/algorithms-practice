15d7f2dd2796afde7d7b1e0e076971ac4f8034ad
RMS <rene.as.himself@gmail.com>
2019-07-29 17:07:26 -0500

    Day 1: Array Intersection

---
+++ solutions/algodaily/day_0_reverse_string.js
@@ -0,0 +1,47 @@
+/*
+    Write a function that reverses an inputted string without using the built-in Array#reverse method. So, for example, calling:
+
+    reverseString("jake") should return "ekaj".
+
+    reverseString("reverseastring") should return "gnirtsaesrever".
+*/
+
+function reverseStringOneLiner(str) {
+    return str // [...str] to avoid split
+        .split('')
+        .reverse()
+        .join('')
+}
+
+function reverseStringIterative(str) {
+    if (str.length <= 1) {
+        return str
+    }
+
+    if (!str || typeof str !== 'string') {
+        return 'Not a string or string is already reversed'
+    }
+
+    const reversed = []
+    const length = str.length - 1
+
+    for (let i = length; i >= 0; i--) {
+        reversed.push(str[i])
+    }
+
+    return reversed.join('')
+}
+
+function reverseStringRecursive(str) {
+    if (str === '') {
+        return str
+    }
+
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0)
+}
+
+module.exports = {
+    reverseStringIterative,
+    reverseStringOneLiner,
+    reverseStringRecursive,
+}
---
+++ solutions/algodaily/day_1_array_intersection.js
@@ -0,0 +1,29 @@
+/*
+    Can you write a function that takes two inputs and returns their intersection? All element in the final result should be unique.
+*/
+
+// O(n)
+function arrayIntersection(nums1, nums2) {
+    const intersection = {}
+
+    for (const num of nums1) {
+        if (nums2.indexOf(num) !== -1) {
+            intersection[num] = 1
+        }
+    }
+
+    return Object.keys(intersection).map(n => parseInt(n))
+}
+
+// O(n)
+function arrayIntersection2(nums1, nums2) {
+    const set = new Set(nums1)
+    const filteredSet = new Set(nums2.filter(n => set.has(n)))
+
+    return Array.from(filteredSet)
+}
+
+module.exports = {
+    arrayIntersection,
+    arrayIntersection2,
+}
