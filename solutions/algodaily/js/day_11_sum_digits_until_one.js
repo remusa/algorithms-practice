1a3e44ff286503325427352a283489a760064be0
RMS <rene.as.himself@gmail.com>
2019-07-30 17:44:02 -0500

    Day 11: Sum Digits Until One

---
+++ solutions/algodaily/js/day_11_sum_digits_until_one.js
@@ -0,0 +1,45 @@
+/*
+
+*/
+
+function stringArrayToNumber(array) {
+    const arr = [...array]
+    for (let i = 0; i < array.length; i++) {
+        arr[i] = parseInt(array[i])
+    }
+    return arr
+}
+
+function sumArray(array) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum
+}
+
+function sumDigits(n) {
+    if (!n) {
+        return null
+    }
+
+    let strArr = n.toString().split('')
+
+    if (strArr.length <= 1) {
+        return n
+    }
+
+    let res = 0
+    let numArr = []
+    while (strArr.length > 1) {
+        numArr = stringArrayToNumber(strArr)
+        res = sumArray(numArr)
+        strArr = res.toString().split('')
+    }
+
+    return res
+}
+
+module.exports = {
+    sumDigits,
+}
