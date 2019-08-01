3d44134ca7de84855feaaa0fd9b66c86fb93707d
RMS <rene.as.himself@gmail.com>
2019-08-01 14:57:07 -0500

    Updated graph algorithms

--- data_structures/07_graphs/js/graph.js
+++ data_structures/07_graphs/js/graph.js
@@ -4,15 +4,33 @@ class Graph {
         this.numberOfNodes = 0
     }
 
-    addVertex(node) {
-        this.adjacencyList[node] = []
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = []
+        }
+
+        this.adjacencyList[vertex] = []
         this.numberOfNodes++
     }
 
-    addEdge(node1, node2) {
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex, adjacentVertex)
+        }
+
+        delete this.adjacencyList[vertex]
+    }
+
+    addEdge(vertex1, vertex2) {
         // undirected graph
-        this.adjacencyList[node1].push(node2)
-        this.adjacencyList[node2].push(node1)
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
     }
 
     showConnections() {
@@ -23,9 +41,11 @@ class Graph {
             const nodeConnections = this.adjacencyList[node]
             let connections = ''
             let vertex
+
             for (vertex of nodeConnections) {
                 connections += `${vertex} `
             }
+
             printedList.push([node, connections])
         }
 
@@ -53,6 +73,22 @@ myGraph.addEdge('0', '2')
 myGraph.addEdge('6', '5')
 
 console.log(myGraph.showConnections())
+
+const g = new Graph()
+
+g.addVertex('Dallas')
+g.addVertex('Tokyo')
+g.addVertex('Aspen')
+g.addVertex('Los Angeles')
+g.addVertex('Hong Kong')
+g.addEdge('Dallas', 'Tokyo')
+g.addEdge('Dallas', 'Aspen')
+g.addEdge('Hong Kong', 'Tokyo')
+g.addEdge('Hong Kong', 'Dallas')
+g.addEdge('Los Angeles', 'Hong Kong')
+g.addEdge('Los Angeles', 'Aspen')
+
+console.log(g.showConnections())
 
 module.exports = {
     Graph,
--- tips/problem_patterns.md
+++ tips/problem_patterns.md
@@ -1,43 +1,6 @@
 # Problem Patterns
 
-PROBLEM SOLVING:
 
-- Understand the Problem
-- Explore Concrete Examples
-- Break It Down
-- Solve/Simplify
-- Look Back and Refactor
-
-## Step 1: Understand the Problem
-
-1. Can I restate the problem in my own words?
-2. What are the inputs that go into the problem?
-3. What are the outputs that should come from the solution to the problem?
-4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you set about solving the problem. That's okay; it's still worth considering the question at this early stage.)
-5. How should I label the important pieces of data that are a part of the problem?
-
-## Step 2: Explore Concrete Examples
-
-* Coming up with examples can help you understand the problem better
-* Examples also  provide sanity checks that your eventual solution works how it should
-* User Stories!
-* Unit Tests!
-
-- Steps:
-  - Start with Simple Examples
-  - Progress to More Complex Examples
-  - Explore Examples with Empty Inputs
-  - Explore Examples with Invalid Inputs
-  - Write a function which takes in a string and returns counts of each character in
-  - the string.
-
-## Step 3: Break It Down
-
-
-## Step 4: Solve/Simplify
-
-
-## Step 5: Look Back and Refactor
 
 
 ## Resources
