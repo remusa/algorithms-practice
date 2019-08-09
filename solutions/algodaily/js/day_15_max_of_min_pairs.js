bcc888c2a74dbcd1938c33daa99204eb73ac65cf
RMS <rene.as.himself@gmail.com>
2019-08-08 20:23:24 -0500

    Day 15: Implement a Linked List

--- solutions/algodaily/js/day_15_max_of_min_pairs.js
+++ solutions/algodaily/js/day_15_max_of_min_pairs.js
@@ -1,5 +1,17 @@
 /*
+    We have an array of length 2 * n (even length) that consists of random integers.
 
+    [1, 3, 2, 6, 5, 4]
+
+    We are asked to create pairs out of these integers, like such:
+
+    [[1, 3], [2, 6], [5, 4]]
+
+    How can we divide up the pairs such that the sum of smaller integers in each pair is maximized?
+
+    Here's an example input: [3, 4, 2, 5]. The return value is 6 because the maximum sum of pairs is 6 = min(2, 3) + min(4, 5).
+
+    Note that negative numbers may also be included.
 */
 
 function maxOfMinPairs(nums) {
---
+++ solutions/algodaily/js/day_31_implement_a_linked_list.js
@@ -0,0 +1,88 @@
+/*
+    Traditionally we have the following LinkedListNode class definition:
+
+    class LinkedListNode {
+        constructor(val, next = null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    ​
+    We can readily create a entire linked list by writing statements like:
+
+    const list1 = new LinkedListNode(2);
+    list1.next =  new LinkedListNode(3);
+    ​
+    This will append the node to the end of our new list, and does the job most of the time. However, what if we wanted to make things cleaner and implement a class to help us append and prepend nodes?
+
+    Given the following skeleton for a Linked List class, can you fill in the #append and #prepend methods? We will call .head on an instance of your class to get the head node.
+
+    class LinkedList {
+        constructor() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        prepend(value) {}
+
+        append(value) {}
+    };
+*/
+
+class LinkedListNode {
+    constructor(val, next = null) {
+        this.val = val
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+
+    prepend(value) {
+        const newNode = new LinkedListNode(value)
+        const oldHead = this.head
+
+        this.head = newNode
+        newNode.next = oldHead
+
+        if (!this.tail) {
+            this.tail = newNode
+        }
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value)
+        const oldTail = this.tail
+
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+            return
+        }
+
+        oldTail.next = newNode
+        this.tail = newNode
+    }
+
+    toString() {
+        // helper method
+        const toPrint = []
+        let currNode = this.head
+
+        while (currNode) {
+            toPrint.push(currNode.val)
+            currNode = currNode.next
+        }
+
+        return toPrint.join(' -> ')
+    }
+}
+
+module.exports = {
+    LinkedList,
+    LinkedListNode,
+}
