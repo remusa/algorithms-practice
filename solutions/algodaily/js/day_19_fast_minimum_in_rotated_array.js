35174a7935beca312b334629bcf6bdebd8825e31
RMS <rene.as.himself@gmail.com>
2019-08-15 14:41:55 -0500

    Removed comments

--- solutions/algodaily/js/day_19_fast_minimum_in_rotated_array.js
+++ solutions/algodaily/js/day_19_fast_minimum_in_rotated_array.js
@@ -32,11 +32,6 @@ function getMinimum(nums) {
     return nums[start]
 }
 
-getMinimum([6, 7, 8, 0, 1, 2, 3, 4, 5]) // 0
-getMinimum([6, 7, 8, 9, 10, 3, 4, 5]) // 3
-getMinimum([4, 5, 1, 2, 3]) // 1
-getMinimum([5, 6, 7, 0, 1, 2, 3]) // 0
-
 module.exports = {
     getMinimum,
 }
