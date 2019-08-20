c5f930c0afd49bc378a9dee8000172433773641b
RMS <rene.as.himself@gmail.com>
2019-08-19 23:45:02 -0500

    Fixed Day 43 tests

--- solutions/algodaily/js/__tests__/day_43_stock_buy_and_sell_optimization.test.js
+++ solutions/algodaily/js/__tests__/day_43_stock_buy_and_sell_optimization.test.js
@@ -3,7 +3,7 @@ import { stockOptimizer } from '../day_43_stock_buy_and_sell_optimization'
 describe('stock optimization tests', () => {
     it('should return the value of the max profit after buying and selling stock', () => {
         expect(stockOptimizer([10, 7, 6, 2, 9, 4])).toBe(7)
-        expect(stockOptimizer(stockOptimizer([9, 8, 6, 5, 3]))).toBe(0)
+        expect(stockOptimizer([9, 8, 6, 5, 3])).toBe(0)
         expect(stockOptimizer([14, 20, 9])).toBe(6)
         expect(stockOptimizer([5, 20, 15, 13, 3, 15, 5, 10])).toBe(15)
         expect(stockOptimizer([1, 6, 11, 2])).toBe(10)
