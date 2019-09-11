0ef92226dc987b68c2df2c91a7cf62436ae955e8
RMS <rene.as.himself@gmail.com>
2019-09-11 13:50:42 -0500

    252. Meeting Rooms

--- solutions/leetcode/LC252_canAttendMeetings/index.js
+++ solutions/leetcode/LC252_canAttendMeetings/index.js
@@ -1,11 +1,44 @@
-// Given an array of meeting time intervals consisting of start and end
-// times [s1, e1], [s2, e2], ... , determine if a person could attend all meetings.
-// ---------------------------
-// canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
-// canAttendMeetings([[7, 10], [2, 4]]) --> true
+/*
+252. Meeting Rooms
+Easy
+https://leetcode.com/problems/meeting-rooms
+https://leetfree.com/problems/meeting-rooms.html
+
+Given an array of meeting time intervals consisting of start and end
+times [s1, e1], [s2, e2], ... , determine if a person could attend all meetings.
+---------------------------
+canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
+canAttendMeetings([[7, 10], [2, 4]]) --> true
+*/
 
 // Time complexity: O(n) ->
 // Space complexity: O(n) ->
-function canAttendMeetings(intervals) {}
+function canAttendMeetings(intervals) {
+    const starts = []
+    const ends = []
+
+    // Fill start and end arrays
+    for (let i = 0; i < intervals.length; i++) {
+        const subArr = intervals[i]
+        starts.push(subArr[0])
+        ends.push(subArr[1])
+    }
+
+    // Sort arrays
+    starts.sort((a, b) => a - b)
+    ends.sort((a, b) => a - b)
+
+    // Check next start with current end
+    for (let i = 0; i < starts.length; i++) {
+        const nextStart = starts[i + 1]
+        const currEnd = ends[i]
+
+        if (nextStart < currEnd) {
+            return false
+        }
+    }
+
+    return true
+}
 
 module.exports = canAttendMeetings
--- tips/intervals.md
+++ tips/intervals.md
@@ -7,3 +7,11 @@
 * Check the next interval, if the next interval intersects with the last interval in the `results` array, extend the last interval in the `results` array to the end of the current interval.
 * If the next interval is shorter than the last interval in the `results` array, we don't need to do anything because it's already included in the last interval.
 * Check the next interval, if it doesn't intersect with the last interval in the `results` array, push it to the `results` array.
+
+## Can Attend Meetings
+
+* Put `start` and `end` times in two separate arrays and sort them.
+* Loop through the `starts` array.
+* Check if `start` time one index ahead is less than end time at current index.
+* If element at one index ahead is smaller than the `end` array at the current index, return `false` since he can't attend both meetings (there's an intersection).
+* Otherwise return `true`.
