43c48249fc84679d3b3c32155ae8ed9cd4788f3e
RMS <rene.as.himself@gmail.com>
2019-09-11 18:59:10 -0500

    Trees and graphs notes

--- solutions/leetcode/LC226_invertTree/index.js
+++ solutions/leetcode/LC226_invertTree/index.js
@@ -1,3 +1,36 @@
+/*
+226. Invert Binary Tree
+Easy
+https://leetcode.com/problems/invert-binary-tree/
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+    Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+*/
+
+// Time complexity: O(n) ->
+// Space complexity: O(1) ->
 function invertTree(root) {}
 
-module.exports = invertTree;
+module.exports = invertTree
--- solutions/leetcode/LC235_lowestCommonAncestor/test.js
+++ solutions/leetcode/LC235_lowestCommonAncestor/test.js
@@ -1,74 +1,74 @@
-const lowestCommonAncestor = require("./index");
+const lowestCommonAncestor = require('./index')
 
 class Node {
-  constructor(value) {
-    this.val = value;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(value) {
+        this.val = value
+        this.left = null
+        this.right = null
+    }
 }
 
 class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-  insert(value) {
-    let newNode = new Node(value);
-    if (!this.root) {
-      this.root = newNode;
-      return;
+    constructor() {
+        this.root = null
     }
-    let current = this.root;
-    while (true) {
-      if (value === current.val) {
-        return;
-      }
-      if (value < current.val) {
-        if (!current.left) {
-          current.left = newNode;
-          return;
-        } else {
-          current = current.left;
+
+    insert(value) {
+        const newNode = new Node(value)
+        if (!this.root) {
+            this.root = newNode
+            return
         }
-      } else {
-        if (!current.right) {
-          current.right = newNode;
-          return;
-        } else {
-          current = current.right;
+        let current = this.root
+        while (true) {
+            if (value === current.val) {
+                return
+            }
+            if (value < current.val) {
+                if (!current.left) {
+                    current.left = newNode
+                    return
+                }
+                current = current.left
+            } else {
+                if (!current.right) {
+                    current.right = newNode
+                    return
+                }
+                current = current.right
+            }
         }
-      }
     }
-  }
-  find(value) {
-    let current = this.root;
-    while (current) {
-      if (value === current.val) {
-        return current;
-      }
-      current = value < current.val ? current.left : current.right;
+
+    find(value) {
+        let current = this.root
+        while (current) {
+            if (value === current.val) {
+                return current
+            }
+            current = value < current.val ? current.left : current.right
+        }
+        return false
     }
-    return false;
-  }
 }
 
-test("Returns lowest common ancestor.", () => {
-  const BST = new BinarySearchTree();
-  BST.insert(8);
-  BST.insert(3);
-  BST.insert(10);
-  BST.insert(1);
-  BST.insert(6);
-  BST.insert(14);
-  //          8
-  //        /   \
-  //       3     10
-  //      / \      \
-  //     1   6     14
-  const node3 = BST.find(3);
-  const node10 = BST.find(10);
-  const node14 = BST.find(14);
+test('Returns lowest common ancestor.', () => {
+    const BST = new BinarySearchTree()
+    BST.insert(8)
+    BST.insert(3)
+    BST.insert(10)
+    BST.insert(1)
+    BST.insert(6)
+    BST.insert(14)
+    //          8
+    //        /   \
+    //       3     10
+    //      / \      \
+    //     1   6     14
+    const node3 = BST.find(3)
+    const node10 = BST.find(10)
+    const node14 = BST.find(14)
 
-  expect(lowestCommonAncestor(BST.root, node3, node10)).toEqual(BST.root);
-  expect(lowestCommonAncestor(BST.root, node10, node14)).toEqual(node10);
-});
+    expect(lowestCommonAncestor(BST.root, node3, node10)).toEqual(BST.root)
+    expect(lowestCommonAncestor(BST.root, node10, node14)).toEqual(node10)
+})
