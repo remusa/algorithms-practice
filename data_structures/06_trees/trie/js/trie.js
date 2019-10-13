aad770f46e1ae7a8758356a35b2631a7844a7d64
RMS <rene.as.himself@gmail.com>
2019-10-12 22:10:39 -0500

    Update trie.js

--- data_structures/06_trees/trie/js/trie.js
+++ data_structures/06_trees/trie/js/trie.js
@@ -1,89 +1,129 @@
 class Node {
-    constructor(end = false) {
+    constructor(value) {
+        this.value = value
         this.children = new Map()
-        this.end = end
-    }
-
-    setEnd() {
-        this.end = true
-    }
-
-    isEnd() {
-        return this.end
+        this.isCompleteWord = false
+        this.prefixes = 0
     }
 }
 
 class Trie {
     constructor() {
-        this.root = new Node()
+        this.root = new Node('')
     }
 
-    // recursive
-    add(input, node = this.root) {
-        // We are at the end of the word
-        if (input.length === 0) {
-            node.setEnd()
+    add(word) {
+        if (!this.root) {
+            return null
         }
-        // If there's more than 0 letters passed to the add function (not at the
-        // end)
-        // If there's not a node with the letter passed, create a node with it
-        else if (!node.children.has(input[0])) {
-            node.children.set(input[0], new Node())
-            return this.add(input.substr(1), node.children.get(input[0]))
+
+        this._addNode(this.root, word)
+    }
+
+    _addNode(node, word) {
+        if (!node || !word) {
+            return null
         }
-        // If there's already a node with that letter
-        else {
-            return this.add(input.substr(1), node.children.get(input[0]))
+
+        node.prefixes++
+
+        const char = word.charAt(0)
+        let child = node.children.get(char)
+
+        // If character doesn't have children
+        if (!child) {
+            child = new Node(char)
+            node.children.set(char, child)
+        }
+
+        const restOfWord = word.substring(1)
+        console.log('restOfWord', restOfWord)
+
+        // If there're no more characters, word is finished
+        if (!restOfWord) {
+            child.isCompleteWord = true
+        }
+
+        this._addNode(child, restOfWord)
+    }
+
+    remove(word) {
+        if (!this.root) {
+            return null
+        }
+
+        if (this.root.children.has(word)) {
+            this._removeNode(this.root, word)
         }
     }
 
-    find(input) {
-        let node = this.root
-        let word = input
+    _removeNode(node, word) {
+        if (!node || !word) {
+            return undefined
+        }
 
-        while (word.length > 1) {
-            // If there isn't a node with that letter
-            if (!node.children.has(word[0])) {
-                return false
+        node.prefixes--
+
+        const char = word.charAt(0)
+        const child = node.children.get(char)
+
+        // If the character exists
+        if (child) {
+            const remainder = word.substring(1)
+
+            // If there's more characters in the word
+            if (remainder) {
+                if (child.prefixes === 1) {
+                    node.children.delete(char)
+                } else {
+                    this._removeNode(child, remainder)
+                }
             }
-            // Else, there must be a word with that letter
-            // Update the node
-            node = node.children.get(word[0])
-            // Set the node to the first character
-            word = word.substr(1)
+            // This is the last character
+            else {
+                if (child.prefixes === 0) {
+                    node.children.delete(char)
+                } else {
+                    child.isCompleteWord = false
+                }
+            }
+        }
+    }
+
+    search(word) {
+        console.log(`word: ${word}`)
+
+        if (!this.root) {
+            console.log('found? ', false)
+            return false
         }
 
-        if (node.children.has(word) && node.children.get(word).isEnd()) {
+        return this._search(this.root, word)
+    }
+
+    _search(node, word) {
+        if (!node || !word) {
+            console.log('found? ', false)
+            return false
+        }
+
+        const char = word.charAt(0)
+        const child = node.children.get(char)
+
+        if (!child) {
+            console.log('found? ', false)
+            return false
+        }
+
+        const remainder = word.substring(1)
+
+        if (!remainder && child.isCompleteWord) {
+            console.log('found? ', true)
             return true
         }
-
-        return false
+        return this._search(child, remainder)
     }
 
-    remove(word) {}
-
-    /*
-        SECOND IMPLEMENTATION
-    */
-
-    insert(word) {
-        let node = this.root
-
-        for (let i = 0; i < word.length; i++) {
-            const char = word[i]
-
-            if (!node.children.has(char)) {
-                node.children.set(char, new Node())
-            }
-
-            node = node.children.get(char)
-        }
-
-        node.setEnd()
-    }
-
-    // Search a prefix or whole key in trie and
-    // returns the node where search ends
     searchPrefix(word) {
         let node = this.root
 
@@ -100,17 +140,6 @@ class Trie {
         return node
     }
 
-    // Returns if the word is in the trie
-    search(word) {
-        const node = this.searchPrefix(word)
-        // Check that the node exists and it is the end of the word
-        console.log('found? ', node !== null && node.isEnd())
-        return node !== null && node.isEnd()
-    }
-
-    /*
-        Search for a key prefix in a trie (searching for a prefix of a key, not for a whole key)
-    */
     startsWith(prefix) {
         const node = this.searchPrefix(prefix)
         return node !== null
@@ -148,6 +177,10 @@ const trie = new Trie()
 trie.add('cherry')
 console.log(trie)
 trie.search('cherry')
-// trie.add('cherry')
+trie.add('cherry')
 trie.search('cheetos')
-// console.log(trie)
+console.log(trie)
+
+module.exports = {
+    Trie,
+}
