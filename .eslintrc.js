f32d88d148871538d7ad2c44eca6833ee6b13eaf
R Sánchez <rene.as.himself@gmail.com>
2019-11-28 19:34:40 -0600

    ESLint and Prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+const prettierConfig = require('./.prettierrc')
+
+module.exports = {
+  extends: ['wesbos'],
+  rules: {
+    'no-lonely-if': 0,
+    'max-classes-per-file': 0,
+    'prefer-destructuring': 0,
+    'no-unused-vars': 0,
+    noshadow: 0,
+    'no-constant-condition': 0,
+    'no-use-before-define': 0,
+    'no-plusplus': 0,
+    'no-continue': 0,
+    'no-param-reassign': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'prettier/prettier': ['error', prettierConfig],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  useTabs: false,
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+}
